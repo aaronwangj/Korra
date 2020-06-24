c12953c82a7e4a7147eaef073490c1ee0ca4ce1d
Aaron <56976471+aaronwang-j@users.noreply.github.com>
2020-06-24 02:52:15 -0400

    Preprocessing (tokenize, stem), bag-of-words model
    Used NLTK to tokenize and stem.
    Implemented bag-of-words model.

---
+++ preprocess.py
@@ -0,0 +1,22 @@
+import nltk
+import numpy as np
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+#nltk.download('punkt') if not done before
+
+ps = PorterStemmer()
+
+def tokenize(query):
+    return nltk.word_tokenize(query)
+
+def stem(w):
+    return ps.stem(w.lower())
+
+def bag_of_words(tokenized_query, all_words):
+    tq = [stem(w) for w in tokenized_query]
+
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for i, word in enumerate(all_words):
+        if word in tq:
+            bag[i] = 1.0
+    return bag
